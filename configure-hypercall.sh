33722354cf56479ce34b8de4ac59f9a332b18ecc
Erik van der Kouwe <erik@minix3.org>
2014-06-29 16:25:52 +0200

    Make right after reconfigure

--- configure-hypercall.sh
+++ configure-hypercall.sh
@@ -4,3 +4,5 @@ mypath="`readlink -f "$0"`"
 mydir="`dirname "$mypath"`"
 #prefixspec=--prefix="`dirname "$mydir"`/install" 
 "$mydir/configure" --target-list=i386-softmmu,x86_64-softmmu $prefixspec --enable-hypercall
+make -j4
+
