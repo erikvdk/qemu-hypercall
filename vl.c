ba965865bdc3abc3da630ccaac85bc8b66ff7126
Erik van der Kouwe <erik@minix3.org>
2014-06-29 18:29:39 +0200

    Report hypercall support with version info

--- vl.c
+++ vl.c
@@ -2058,6 +2058,9 @@ static void main_loop(void)
 static void version(void)
 {
     printf("QEMU emulator version " QEMU_VERSION QEMU_PKGVERSION ", Copyright (c) 2003-2008 Fabrice Bellard\n");
+#ifdef CONFIG_HYPERCALL
+    printf("with hypercall support\n");
+#end
 }
 
 static void help(int exitcode)
