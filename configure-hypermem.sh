c44979249745da5adbbc228cfbe56980c4b576f5
Erik van der Kouwe <erik@minix3.org>
2014-07-02 09:49:06 +0200

    Add hypermem default configuration script

---
+++ configure-hypermem.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mypath="`readlink -f "$0"`"
+mydir="`dirname "$mypath"`"
+#prefixspec=--prefix="`dirname "$mydir"`/install"
+"$mydir/configure" --target-list=i386-softmmu,x86_64-softmmu $prefixspec --enable-hypermem
+make -j4
+
