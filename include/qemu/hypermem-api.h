396b1cce124d712a8e86023961d4f182bbb1235e
Erik van der Kouwe <erik@minix3.org>
2014-07-05 18:54:47 +0200

    Fix conflict

--- include/qemu/hypermem-api.h
+++ include/qemu/hypermem-api.h
@@ -46,12 +46,11 @@
 typedef uint32_t hypermem_entry_t;
 /* I don't know how to tell the OS a memory region is reserved so I'll just
  * steal part of the video memory and hope it won't be used
+ *
+ * note: more than 256 bytes risks a race condition if reads are not atomic 
  */
 #define HYPERMEM_BASEADDR	0xb7000
-#define HYPERMEM_SIZE		0x00100 /* note: more than 256 bytes risks
-				         * a race condition if reads are
-				         * not atomic 
-					 */
+#define HYPERMEM_SIZE		0x00100 
 
 #define HYPERMEM_COMMAND_NOP			1
 #define HYPERMEM_COMMAND_FAULT			2
