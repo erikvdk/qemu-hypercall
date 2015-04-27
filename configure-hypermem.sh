fd7390e9eb591ed51b1f8ef794aed93362162ffb
Erik van der Kouwe <erik@minix3.org>
2015-04-27 15:15:31 +0200

    configure-hypermem.sh: install to subdirectory

--- configure-hypermem.sh
+++ configure-hypermem.sh
@@ -3,5 +3,7 @@
 mypath="`readlink -f "$0"`"
 mydir="`dirname "$mypath"`"
 #prefixspec=--prefix="`dirname "$mydir"`/install"
-"$mydir/configure" --target-list=i386-softmmu,x86_64-softmmu $prefixspec --enable-hypermem
+"$mydir/configure" --target-list=i386-softmmu,x86_64-softmmu $prefixspec --enable-hypermem --prefix="$mydir/../bin"
 make -j4
+make install
+
